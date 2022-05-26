0bbe53faf8eab67c2b900288862bb00e5ef94377
Erwin <erwin.cca@gmail.com>
2022-05-26 14:02:37 -0400

    v1.01

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get("/", async (req, res) => {
 
 //Ruta para registrarse
 app.get("/register", (req, res) => {
-    res.render('registro', { layout: "registro" })
+    res.render('registro', { layout: "Registro" })
 })
 
 //Ruta para iniciar sesión
